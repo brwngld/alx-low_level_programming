61f52f0728fd78b6e140733be7ec9b97fc5eaf4e
brwngld <bern587@yahoo.com>
2023-03-17 02:41:27 -0700

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints to string
+ * Description: Prints "and that piece of art is useful.." without puts
+ * Return: 1
+ */
+
+int main(void)
+{
+	fprintf("and that piece of art is useful\n");
+	fprintf("- Dora Korpar, 2015-10-19\n");
+	return (1);
+}
+
