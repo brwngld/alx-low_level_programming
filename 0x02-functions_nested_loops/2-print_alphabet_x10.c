3e62306ffc4062754c7f7676d78611c571afb755
brwngld <bern587@yahoo.com>
2023-03-21 13:53:06 -0700

    Print Alphabet ten times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,16 +5,16 @@
  *
 */
 
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	int ten = 0;
 	char alpha;
-	
+
 	while (ten++ <= 9)
 	{
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 		_putchar(alpha);
-	
+
 	_putchar('\n');
 	}
 }
