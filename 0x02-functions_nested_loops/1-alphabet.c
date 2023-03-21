866847a97c8ce9602a09572ecec4be98697f4c76
brwngld <bern587@yahoo.com>
2023-03-21 06:38:07 -0700

    Print Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - pprint all alphabeth in lowercase
+ * written by brwngld
+*/
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		-putchar(letter);
+
+	-putchar('\n);
+}
