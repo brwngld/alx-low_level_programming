22e599925f86b29cfcdfd915062b80917ba24ffa
brwngld <bern587@yahoo.com>
2023-03-23 13:59:39 -0700

    Testing function

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * postitive_or_negative - tests function that prints if interger is positive or negative.
+ *
+ * Return: 0
+*/
+
+void postive_or_negative(int);
+{
+	 int i;
+	 
+	 i = 98;
+	 positive_or_negative(i);
+	 
+	 return (0);
+}
