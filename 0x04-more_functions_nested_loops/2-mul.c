3027e3e8fb369fcd35e6ee281048225c34b56c81
brwngld <bern587@yahoo.com>
2023-03-23 18:01:27 -0700

    2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,15 +1,13 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
  * mul - A function that multiplies two integers.
- * written by brwngld
  * @a: integer
  * @b: integer
  * Return: multiplication of a and b
 */
 
-int mul(int a, int b);
+int mul(int a, int b)
 {
 	return (a * b);
 }
