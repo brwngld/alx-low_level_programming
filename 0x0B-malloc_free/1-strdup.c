76012af6ed7d895f04518fb002261a5c746124f0
brwngld <bern587@yahoo.com>
2023-04-16 06:36:02 -0700

    Task 3

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,19 +1,21 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * _strdup - Returns a dynamically allocated copy of the input
  * string @str
  *
  * @str:  The input string to be copied
- * Return: Alwasys 0
+ * Return: A pointer to the new copy of the input string
 */
 
 char *_strdup(char *str)
 {
 	size_t len;
 	char *new_str;
+	size_t i;
 
 	len = strlen(str);
 	new_str = malloc(len + 1);
@@ -22,7 +24,8 @@ char *_strdup(char *str)
 	{
 		return (NULL);
 	}
-	for (size_t i = 0; i <= len; i++)
+
+	for (i = 0; i <= len; i++)
 	{
 		new_str[i] = str[i];
 	}
