eec34bd50853a7b0e20fe659820709604048ece4
brwngld <bern587@yahoo.com>
2023-04-18 15:14:19 -0700

    Dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * init_dog - Prints out the information of poppy
+ * @d: Pointer
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner's name
+ * Return: void
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name;
+		d->age;
+		d->owner;
+	}
+}
