bfa0dc08a490a7e001717a0faa7a6169ef19b75a
brwngld <bern587@yahoo.com>
2023-04-18 15:06:59 -0700

    Dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Poppy details
+ *@name: Name of Poppy
+ *@age: Age of Poppy
+ *@owner: Owner of Poppy
+ *
+ * Description: This struct takes the name, age and owner's name
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
