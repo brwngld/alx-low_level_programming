e98b0f6760fb3166257401c57b003018b6f5746a
brwngld <bern587@yahoo.com>
2023-04-19 08:08:39 -0700

    Task 4

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Multiplies two numbers.
+ * @argc: Argument count
+ * @argv: Arguments
+ *
+ * Return: 0 Always
+*/
+
+int main(int argc, char **argv)
+{
+	int x, y;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+
+	return (0);
+}
